d348a906c74abc82831d5d9b8ccf02b6688be950
AMORE76 <gemenius007@internet.ru>
2022-09-16 17:06:31 +0300

    Created block of TechnicalTask

--- Program.cs
+++ Program.cs
@@ -9,26 +9,3 @@
 [“Russia”, “Denmark”, “Kazan”] → []
 */
 
-string[] AddNewValueToArray(string[] arr, string newRow)
-{
-    string[] newArr = new string[arr.Length + 1];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        newArr[i] = arr[i];
-    }
-    newArr[newArr.Length - 1] = newRow;
-    return newArr;
-}
-
-string[] InputArrayByUser()
-{
-    string[] arr = new string[0];
-    while (true)
-    {
-        Console.Write("Введите строку(и) с клавиатуры. После завершения ввода введите с клавиатуры - Q. : ");
-        string userInformation = Console.ReadLine()!;
-        if (userInformation == "q" || userInformation == "Q") break;
-        else arr = AddNewValueToArray(arr, newRow: userInformation);
-    }
-    return arr;
-}
